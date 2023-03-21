c9bb6f4aef090d6d789fde1ca6aba956ebb0dc27
lFer17 <lxavez20@gmail.com>
2023-03-21 13:05:26 -0500

    integrated all sub menu in mobile & desktop

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const $ = (selector) => {
-   return document.querySelector(selector)
-} 
+    return document.querySelector(selector)
+}
 
 let navbarEmail = $('.navbar-email');
 
 let desktopSubMenuEmail = $('.desktop-menu');
 
-const displayMenu = () =>{
-    desktopSubMenuEmail.classList.toggle('inactive');   
+let burgerIcon = $('.menu');
+let mobileMenu  = $('.mobile-menu');
+
+let shoppingCart = $('.navbar-shopping-cart');
+let shoppingCartProducts = $('.product-detail');
+
+
+const displayMenu = () => {
+    desktopSubMenuEmail.classList.toggle('inactive');
 }
 
-navbarEmail.addEventListener('click',displayMenu);
+const showMobileMenu = () =>{
+    mobileMenu.classList.toggle('inactive')
+    shoppingCartProducts.classList.add('inactive')
+}
+
+navbarEmail.addEventListener('click', displayMenu);
+
+burgerIcon.addEventListener('click',showMobileMenu);
+
+
+const showShoppingCart = () =>{
+    shoppingCartProducts.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+}
+
+shoppingCart.addEventListener('click',showShoppingCart)
